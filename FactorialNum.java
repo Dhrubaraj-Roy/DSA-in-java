63fc1608863225de5400ef33f26c7688f5fdfb95
Djrubaraj-Roy <dhrubarajroy1233@gmail.com>
2022-06-17 12:35:27 +0530

    1st file

---
+++ FactorialNum.java
@@ -0,0 +1,10 @@
+class  FactorialNum{
+    public static void main(String[] args) {
+        int fact=1;
+        int num=5;
+        for (int i=1; i<=num; i++){
+            fact=fact*i;
+        }
+        System.out.println("the factorial of "+num+" is "+fact);
+    }
+}
